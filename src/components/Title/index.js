034a5cd0c92c7881493e27cbd920e99593f290b7
toanlion <toanlion24@gmail.com>
2024-04-18 01:55:50 +0700

    ReactNative

---
+++ src/components/Title/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+import  styles from './styles';
+
+const Title = () => {
+    return (
+        
+            <Text style={styles.title}>Travel App</Text>
+        
+    );
+}
+
+export default Title;
---
+++ src/components/Title/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    title:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 10
+    }
+})
+
+export default styles;
